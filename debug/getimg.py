d1c13cb01d149b53836e73558c92237ce5b64fd4
Daizo Sasaki <1100594+dzonesasaki@users.noreply.github.com>
2020-08-12 13:07:39 +0900

    Create getimg.py

---
+++ debug/getimg.py
@@ -0,0 +1,14 @@
+import requests
+
+urlSite='http://192.168.1.2:1880' #host
+urlGet='/get'
+urlParam=''
+
+fnames='myimg.png'
+
+myUrl = urlSite + urlGet + urlParam
+
+objGet = requests.get(myUrl)
+
+with open(fnames,'wb') as fid:
+	fid.write(objGet.content)
